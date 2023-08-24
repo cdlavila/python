2e83097d93afb512eea73541042fd1e00a9d9263
Carlos <carlosdaniel.londono@utp.edu.co>
2023-08-24 14:49:37 -0500

    final version

--- 4_lists_slicing.py
+++ 4_lists_slicing.py
@@ -1,3 +1,16 @@
+"""
+In Python, the concept of "slicing" lists and sequences is well-known and follows a consistent syntax.
+The syntax for slicing is as follows:
+
+list[start:stop:step]
+
+Where:
+
+start: The index to start the slice (inclusive).
+stop: The index to end the slice (exclusive).
+step: The step size between elements in the slice (optional, defaults to 1).
+"""
+
 # Define a list
 my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print("Original list:", my_list) # Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -13,3 +26,11 @@ print("Elements from index 5 onwards:", from_four_onwards)  # Output: [5, 6, 7, 
 # Using List[n:m]
 between_three_and_six = my_list[2:5]
 print("Elements between index 2 and 5:", between_three_and_six)  # Output: [2, 3, 4]
+
+# Using List[start:stop:step]
+sliced_list = my_list[1:8:2]
+print("Sliced list with step 2:", sliced_list)  # Output: [1, 3, 5, 7]
+
+# Using List[::step]
+stepped_list = my_list[::2]
+print("List with step 2:", stepped_list)  # Output: [0, 2, 4, 6, 8]
